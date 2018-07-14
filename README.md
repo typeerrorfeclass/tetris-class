71cc3fe7bfbeef3b355e6258807bea1777803976
yusangeng <yusangeng@outlook.com>
2018-07-14 19:27:46 +0800

    添加ppt内容

---
+++ README.md
@@ -0,0 +1,3 @@
+# tetris-class
+
+编程实践 - 网页版小游戏俄罗斯方块（Tetris）开发.
--- ppt/src/ppt.js
+++ ppt/src/ppt.js
@@ -5,6 +5,12 @@ import { HashRouter as Router, Route, Redirect } from 'react-router-dom'
 import Catalog from './component/Catalog'
 import Page from './component/Page'
 import Box from './component/Box'
+import image0 from './img/prototype.png'
+import image1 from './img/mvc.jpg'
+import image2 from './img/twoway.png'
+import image3 from './img/twoway.png'
+import image4 from './img/event-driven.png'
+import image5 from './img/data-driven.png'
 
 export default class PPT extends React.Component {
   static get displayName() {
@@ -37,7 +43,7 @@ export default class PPT extends React.Component {
       currIndex = -1
     }
 
-    if (currIndex >= 4 - 1) {
+    if (currIndex >= 9 - 1) {
       return
     }
 
@@ -66,7 +72,17 @@ export default class PPT extends React.Component {
           }}>
           <div className="pages">
             <Catalog
-              items={['首页', 'TypeError课程介绍', '课程内容', '未命名']}
+              items={[
+                '首页',
+                'TypeError课程介绍',
+                '课程内容',
+                '未命名',
+                '俄罗斯方块到底是个什么游戏？',
+                '俄罗斯方块比“电商购物车”好在哪？',
+                '未命名',
+                '前端MVC',
+                '事件驱动 VS 数据驱动'
+              ]}
             />
             <Page pageIndex={0}>
               <Box data={{ className: 'cover' }}>
@@ -121,10 +137,121 @@ export default class PPT extends React.Component {
               </ul>
             </Page>
             <Page pageIndex={3}>
-              <pre>
-                <code className="language-js">{`const a = 1
-const str = \`hahaha ${a}\` `}</code>
-              </pre>
+              <Box data={{ className: 'cover' }}>
+                <h1 id="-">需求分析</h1>
+              </Box>
+            </Page>
+            <Page pageIndex={4}>
+              <h2 id="-">俄罗斯方块到底是个什么游戏？</h2>
+              <h3 id="-">俄罗斯方块的要素</h3>
+              <ul>
+                <li>界面展示</li>
+                <li>定时刷新</li>
+                <li>键盘响应</li>
+                <li>方块模型</li>
+                <li>游戏规则</li>
+              </ul>
+              <h3 id="-">案例图</h3>
+              <div className="plugin-image">
+                <img src={image0} />
+              </div>
+            </Page>
+            <Page pageIndex={5}>
+              <h2 id="-">俄罗斯方块比“电商购物车”好在哪？</h2>
+              <ul>
+                <li>业务比较简单，人人都了解，不需要过多前置知识</li>
+                <li>技术栈比较单纯，不需要使用过多工具</li>
+                <li>本身的复杂性高于“购物车”</li>
+                <li>可以在成品的基础上进行技术演进，过渡到前端框架</li>
+              </ul>
+              <h3 id="-">重点</h3>
+              <p>
+                重点不在于写出一个俄罗斯方块(任何人动动脑筋都可以做到), 而在于:
+              </p>
+              <ul>
+                <li>通过代码熟悉已经学过的es6语法的使用</li>
+                <li>通过代码掌握前端单页应用的设计套路(MVC)</li>
+              </ul>
+            </Page>
+            <Page pageIndex={6}>
+              <Box data={{ className: 'cover' }}>
+                <h1 id="-">方案设计</h1>
+              </Box>
+            </Page>
+            <Page pageIndex={7}>
+              <h2 id="-mvc">前端MVC</h2>
+              <h3 id="-">架构图</h3>
+              <div className="plugin-image">
+                <img src={image1} />
+              </div>
+              <h3 id="mvc-">MVC分别是什么？</h3>
+              <ul>
+                <li>
+                  M: model, 负责存储数据,
+                  以及实现数据之间的驱动关系(业务逻辑)的模块
+                </li>
+                <li>V: view, 负责展示数据, 接收用户交互事件的模块</li>
+                <li>
+                  C: controller,
+                  负责衔接M和V的模块——将model数据转换成view可以辨认使用的数据,
+                  将用户交互事件转换成model可以理解的业务事件的模块
+                </li>
+              </ul>
+              <h3 id="mvc-">MVC的本质</h3>
+              <p>
+                分层设计, 各司其职, 将M驱动V和V驱动M这两条信息通道隔离开,
+                简化代码实现.
+              </p>
+              <div className="plugin-image">
+                <img src={image2} />
+              </div>
+              <h3 id="-mvc-">前端MVC的特点</h3>
+              <p>
+                controller经常分散或者隐藏在其他模块中(document-view模式、MVVM模式……).
+              </p>
+            </Page>
+            <Page pageIndex={8}>
+              <h2 id="-vs-">事件驱动 VS 数据驱动</h2>
+              <h3 id="-">再回味一下两条信息通道</h3>
+              <div className="plugin-image">
+                <img src={image3} />
+              </div>
+              <p>事件驱动和数据驱动主要是指黑色箭头组成的通道的实现方式.</p>
+              <h3 id="-">事件驱动</h3>
+              <div className="plugin-image">
+                <img src={image4} />
+              </div>
+              <h4 id="-">优点</h4>
+              <ul>
+                <li>灵活、自由</li>
+                <li>减小view层的渲染负担</li>
+              </ul>
+              <h4 id="-">缺点</h4>
+              <ul>
+                <li>代码复杂、不易维护</li>
+                <li>事件爆炸</li>
+              </ul>
+              <h3 id="-">数据驱动</h3>
+              <div className="plugin-image">
+                <img src={image5} />
+              </div>
+              <h4 id="-">优点</h4>
+              <ul>
+                <li>
+                  不需要关注细节, 实现成本低,
+                  易维护(同样的脑力可以写出更复杂的应用)
+                </li>
+              </ul>
+              <h4 id="-">缺点</h4>
+              <ul>
+                <li>对view层的渲染性能要求比较高</li>
+                <li>
+                  数据对应状态, 事件对应过程,
+                  使用数据驱动实现动画等过程比较不直观
+                </li>
+              </ul>
+              <h3 id="-">结论</h3>
+              <p>99.9%的情况下你压根不应该考虑使用事件驱动模型, 忘记它.</p>
             </Page>
             <Route exact path="/" render={_ => <Redirect to={'/0'} />} />
           </div>
